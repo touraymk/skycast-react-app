ae51e5c4348892da1b1e9cc8a121536100879400
touraymk <131713898+touraymk@users.noreply.github.com>
2023-09-07 17:54:34 +0200

    Update Weather.js
    - Added search field to Weather.js and styling with Bootstrap and CSS.
    - Added background video, implemented it to Weather.js and styled with CSS.

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,22 @@
 import React from "react";
 import "./Weather.css";
+import videoFile from "./star-bg-skycast.mp4";
 
 export default function Weather() {
-  return <h1>Hello from Weather</h1>;
+  return (
+    <div className="Weather">
+      <video className="videoBackground shadow-lg" autoPlay muted loop>
+        <source src={videoFile} type="video/mp4"></source>
+      </video>
+      <form>
+        <input
+          type="search"
+          placeholder="Enter city.."
+          className="col-7"
+          autoFocus={true}
+        />
+        <button type="submit">🔍</button>
+      </form>
+    </div>
+  );
 }
