cee8a3a2c39b94272f8c45b04bc6bec16ed1553d
touraymk <131713898+touraymk@users.noreply.github.com>
2023-09-07 16:24:49 +0200

    Update App.js
    Added content to the footer, added link to personal website and GitHub repository.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <footer>
+        Coded with <span className="emojiColor">♥️</span> by{" "}
+        <a href="https://monumental-malasada-41cd9f.netlify.app">
+          Mariama Touray
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/touraymk/skycast-react-app">open-sourced</a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
