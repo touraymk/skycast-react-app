d724657aa7b693690a6d19b2d5ab558a9007ef77
touraymk <131713898+touraymk@users.noreply.github.com>
2023-09-07 17:45:20 +0200

    Added Weather.js/css
    Implemented it to App.js, and styled it with CSS and Bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
+      <div className="container mt-md-5 appWrapper">
+        <Weather />
+      </div>
+
       <footer className="text-center">
         Coded with <span className="emojiColor">♥️</span> by{" "}
         <a href="https://monumental-malasada-41cd9f.netlify.app">
---
+++ src/Weather.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return <h1>Hello from Weather</h1>;
+}
